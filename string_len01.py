bb8a1d52ab732b365cf7313737d921a8330eb14f
samandar <samandar.abduhakimov.007@gmail.com>
2023-11-09 22:18:24 +0500

    ss

--- string_len01.py
+++ string_len01.py
@@ -1,9 +1,2 @@
-def main(a):
-    """
-    A variable of type string is given. Find its length.
-    Args:
-        a: string
-    Returns:
-        length of string
-    """
-    return
+a="python"
+print(len(a))
